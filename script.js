2c76528590931f63fa73854952f1b97a442a7d3d
roxletst <roxana_steinman@hotmail.com>
2022-11-03 01:26:35 -0300

    v1.1 background & font

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-console.log ("ahhhhh")
-
-/* cuentas */
-// $(document).ready(function(){
-//     $('#goRight').on('click', function(){
-//       $('#slideBox').animate({
-//         'marginLeft' : '0'
-//       });
-//       $('.topLayer').animate({
-//         'marginLeft' : '100%'
-//       });
-//     });
-//     $('#goLeft').on('click', function(){
-//       $('#slideBox').animate({
-//         'marginLeft' : '50%'
-//       });
-//       $('.topLayer').animate({
-//         'marginLeft': '0'
-//       });
-//     });
-//   });
-
-/* login */
-
 document.getElementById("btn_registrarse").addEventListener("click", register);
 document.getElementById("btn_iniciar-sesion").addEventListener("click", iniciarSesion);
 document.querySelector(".formulario_login").addEventListener("submit", login);
@@ -51,6 +27,44 @@ function iniciarSesion(){
 }
 
 //Validación de Inicio de Sesión
-function login() {
-    //validar usuario y contraseña
-}
+//validar usuario y contraseña   
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Por favor ingresá una dirección de mail correcta");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+// Cuenta regresiva para sección TV
+
+let fechaCuentaRegresiva = new Date("Ene 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+let cuentaRegresiva = setInterval(function() {
+
+  // Get todays date and time
+  let ahora = new Date().getTime();
+  
+  // Find the distance between now an the count down date
+  let distancia = fechaCuentaRegresiva - ahora;
+  
+  // Time calculations for days, hours, minutes and seconds
+  let dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  let horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+  let segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+  
+  // Output the result in an element with id="demo"
+  document.getElementById("tiempoRestante").innerHTML = dias + "d " + horas + "h "
+  + minutos + "m " + segundos + "s ";
+  
+  // If the count down is over, write some text 
+  if (distancia < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("tiempoRestante").innerHTML = "YA ESTÁ LLEGANDO!";
+  }
+}, 1000);
